7bab674b3c63161e904eac4fdf72918ab313ad3a
EdwinPoueriet <edwinpoueriet.dev@gmail.com>
2022-02-14 14:46:48 -0400

    fixing ruler bugs

--- rock-paper-scissors/pages/index.js
+++ rock-paper-scissors/pages/index.js
@@ -8,28 +8,36 @@ import styles from '../styles/Home.module.css'
 // Scissors beats Paper
 
 export default function Home() {
+  const opt = [
+      {id: 0, option:"paper", beats: [1]},
+      {id: 1, option:"rock", beats:[2]},
+      {id: 2, option:"scissors", beats:[0]}
+  ]
   const ruler = (entry) => {
-    const opt = ["paper","rock","scissors"]
     const userSelection = entry
     const machineSelection = Math.floor(Math.random() * opt.length)
-    console.log(opt[entry],"====>", opt[machineSelection])
-    if(userSelection === machineSelection){
-      console.log("tied")
-      return
-    }
-    if(userSelection < machineSelection && userSelection !== opt.length ){
-      console.log("you won")
-      return
-    }
-    if(userSelection > machineSelection || userSelection !== opt.length){
-      console.log("you lost")
+    console.log(userSelection.beats)
+    console.log(userSelection.option,"->", opt[machineSelection].option)
 
+    if(userSelection.id === machineSelection) {
+       console.log("tied")
+       return
+    }
+    if(userSelection.beats.some(opt => opt !== machineSelection) ){
+      console.log("you lost")
+    }else{
+       console.log("you won")
     }
 
   }
   return (
    <>
-      <button onClick={() => {ruler(1)}}>play</button>
+     { opt.map((item) =>
+       <button key={item.id} onClick={() => {ruler(item)}}>{item.option}</button>
+     )}
+
    </>
   )
 }
+
+
