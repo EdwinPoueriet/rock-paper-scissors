167a465bb7b41d78978395dda9a0ee0af6a3e587
EdwinPoueriet <edwinpoueriet.dev@gmail.com>
2022-02-16 09:07:24 -0400

    adding header with global css and module.css

--- rock-paper-scissors/pages/index.js
+++ rock-paper-scissors/pages/index.js
@@ -1,13 +1,8 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import logo from '../public/images/logo.svg'
 import useLocalStorage from "../hooks/useLocalStorage";
 import {useState, useEffect} from "react";
 
-
-// Paper beats Rock
-// Rock beats Scissors
-// Scissors beats Paper
 
 export default function Home() {
   const opt = [
@@ -18,36 +13,41 @@ export default function Home() {
 
   const [score, setScore] = useLocalStorage("Score",0);
   const [result, setResult] = useState("")
+
   const ruler = (entry) => {
     const userSelection = entry
     const machineSelection = Math.floor(Math.random() * opt.length)
 
     if(userSelection.id === machineSelection) {
-       console.log("tied")
-       console.log(score);
        setResult("Tied")
        return
     }
     if(userSelection.beats.some(opt => opt !== machineSelection) ){
-        console.log("you lost")
         setScore(score - 1)
         setResult("You Lost")
     }else{
-        console.log("you won")
         setScore(score + 1)
         setResult("You Won")
     }
-    console.log("score",score)
-
   }
+
   return (
    <>
-     { opt.map((item) =>
-       <button key={item.id} onClick={() => {ruler(item)}}>{item.option}</button>
-     )}
-       score: {score}
-       Result: {result}
-       <button onClick={() => {setScore(0)}}>Restart</button>
+     {/*{ opt.map((item) =>*/}
+     {/*  <Button key={item.id} sx={ { borderRadius: 50, backgroundColor: '#cfe8fc' } } onClick={() => {ruler(item)}}>{item.option}</Button>*/}
+     {/*)}*/}
+     {/*  score: {score}*/}
+     {/*  Result: {result}*/}
+     {/*  <button onClick={() => {setScore(0)}}>Restart</button>*/}
+       <div className={styles.container}>
+           <header className={styles.header}>
+               <img className={styles.logo} src={logo} alt="Rock, Paper, Scissors"/>
+               <div className={styles.score_container}>
+                   <h4 className={styles.score_title}>SCORE</h4>
+                   <p className={styles.score}>0</p>
+               </div>
+           </header>
+       </div>
    </>
   )
 }
